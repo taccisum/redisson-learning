d0dcffbb1e154754ad92ea0a213040203d9683b4
taccisum <514162920@qq.com>
2018-09-21 16:21:07 +0800

    using embedded redis

--- src/test/java/com/github/taccisum/learning/redission/GettingStartTest.java
+++ src/test/java/com/github/taccisum/learning/redission/GettingStartTest.java
@@ -5,10 +5,16 @@ import org.junit.runner.RunWith;
 import org.redisson.api.RAtomicLong;
 import org.redisson.api.RKeys;
 import org.redisson.api.RedissonClient;
+import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.embedded.RedisServer;
 
 /**
  * @author tac - liaojf@cheegu.com
@@ -17,9 +23,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @ActiveProfiles("getting-start")
+@ImportAutoConfiguration(GettingStartTest.Config.class)
 public class GettingStartTest {
     @Autowired
     private RedissonClient redissonClient;
+
+    @TestConfiguration
+    @AutoConfigureBefore(RedissonAutoConfiguration.class)
+    public static class Config {
+        @Bean(destroyMethod = "stop")
+        public RedisServer redisServer() {
+            RedisServer redisServer = RedisServer.builder().setting("maxheap 32M").port(6379).build();
+            redisServer.start();
+            return redisServer;
+        }
+    }
 
     @Test
     public void keys() throws Exception {
