7022e7cee46ed6f7a37aa20ab8a3f952c8b69ad6
taccisum <514162920@qq.com>
2018-09-21 16:34:23 +0800

    bucket sample

--- src/test/java/com/github/taccisum/learning/redission/GettingStartTest.java
+++ src/test/java/com/github/taccisum/learning/redission/GettingStartTest.java
@@ -3,6 +3,7 @@ package com.github.taccisum.learning.redission;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.redisson.api.RAtomicLong;
+import org.redisson.api.RBucket;
 import org.redisson.api.RKeys;
 import org.redisson.api.RedissonClient;
 import org.redisson.spring.starter.RedissonAutoConfiguration;
@@ -15,6 +16,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import redis.embedded.RedisServer;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author tac - liaojf@cheegu.com
@@ -52,5 +55,44 @@ public class GettingStartTest {
                 .whenComplete((res, e) -> {
                     System.out.println(res);
                 });
+    }
+
+    @Test
+    public void bucket() throws Exception {
+        RBucket<FooModel> foo = redissonClient.getBucket("foo");
+        assertThat(foo.isExists()).isFalse();
+        foo.set(new FooModel("tac", 123L));
+        assertThat(foo.isExists()).isTrue();
+        assertThat(foo.get().getFoo1()).isEqualTo("tac");
+        assertThat(foo.get().getFoo2()).isEqualTo(123L);
+    }
+
+    public static class FooModel {
+        private String foo1;
+        private Long foo2;
+
+        public FooModel() {
+        }
+
+        public FooModel(String foo1, Long foo2) {
+            this.foo1 = foo1;
+            this.foo2 = foo2;
+        }
+
+        public String getFoo1() {
+            return foo1;
+        }
+
+        public void setFoo1(String foo1) {
+            this.foo1 = foo1;
+        }
+
+        public Long getFoo2() {
+            return foo2;
+        }
+
+        public void setFoo2(Long foo2) {
+            this.foo2 = foo2;
+        }
     }
 }
