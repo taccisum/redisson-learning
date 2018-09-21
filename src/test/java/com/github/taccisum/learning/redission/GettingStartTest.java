674add16a525758b898db88da778d5da9d287b6d
taccisum <514162920@qq.com>
2018-09-21 15:30:28 +0800

    move getting start to unit test

--- src/test/java/com/github/taccisum/learning/redission/GettingStartTest.java
+++ src/test/java/com/github/taccisum/learning/redission/GettingStartTest.java
@@ -1,23 +1,25 @@
-package com.github.taccisum.learning.redission.runner;
+package com.github.taccisum.learning.redission;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.redisson.api.RKeys;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author tac - liaojf@cheegu.com
  * @since 2018/9/21
  */
-@Component
-public class FooRunner implements ApplicationRunner {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class GettingStartTest {
     @Autowired
     private RedissonClient redissonClient;
 
-    @Override
-    public void run(ApplicationArguments applicationArguments) throws Exception {
+    @Test
+    public void keys() throws Exception {
         if (redissonClient.isShutdown()) {
             System.out.println("redission is shutdown now");
         }
